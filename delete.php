1f6c104510ac61ac5b4ce7e8ae53b6fcdf539538
Shoaib Mohammad Ankon <shoaib.ankon@gmail.com>
2019-07-29 15:36:06 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,11 +3,6 @@ $con=mysqli_connect("localhost","root","","ipasso") or die ("Not connected");
 $sql="truncate table cata";
 $query=mysqli_query($con,$sql);
 
-
-
-
-
-
-
+header("Location: index.php");
 
 ?>
